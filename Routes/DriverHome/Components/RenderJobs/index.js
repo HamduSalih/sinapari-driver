fa68e525dc1b76240b6cbd9b56dd812326c212dd
HamduSalih <afa.hamdu@gmail.com>
2020-02-08 15:17:08 +0000

    new commit

--- Routes/DriverHome/Components/RenderJobs/index.js
+++ Routes/DriverHome/Components/RenderJobs/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, SafeAreaView, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
 
 const RenderJobs = ({allJobs}) => {
         var jobStore = Object.entries(allJobs);
     return(
-        <Text style={{fontSize:30, position:'absolute'}}>{jobStore[0][1].client}</Text>
+        <SafeAreaView style = {styles.container}>
+            <ScrollView style = {styles.scrollView}>        
+                <Text style={{fontSize:30, position:'absolute'}}>{jobStore[0][1].client}</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: Constants.statusBarHeight,
+    },
+    scrollView: {
+      backgroundColor: 'pink',
+      marginHorizontal: 20,
+    },
+});
 
 export default RenderJobs;
